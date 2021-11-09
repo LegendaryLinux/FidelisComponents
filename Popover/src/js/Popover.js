c24b91f5a3f813868df0928d54d6634fe9b11085
Chris Wilson <chris@legendserver.info>
2021-11-09 15:48:06 -0500

    Swap Popover to two-space indentation

--- Popover/src/js/Popover.js
+++ Popover/src/js/Popover.js
@@ -10,81 +10,81 @@ import '../styles/Popover.scss';
  * Props content Anything passed in on this prop will be rendered in a contentId Div
  */
 class Popover extends Component {
-	constructor(props) {
-		super(props);
-		this.triggerRef = React.createRef();
-		this.contentRef = React.createRef();
-		this.popperInstance = null;
-		this.state = {
-			popoverVisible: false,
-		};
-	}
+  constructor(props) {
+    super(props);
+    this.triggerRef = React.createRef();
+    this.contentRef = React.createRef();
+    this.popperInstance = null;
+    this.state = {
+      popoverVisible: false,
+    };
+  }
 
-	componentDidMount() {
-		const trigger = this.triggerRef.current;
-		const content = this.contentRef.current;
-		this.popperInstance = createPopper(trigger, content, {
-			modifiers: [
-				{
-					name: 'offset',
-					options: {
-						offset: [0, 8],
-					},
-				}
-			],
-		});
+  componentDidMount() {
+    const trigger = this.triggerRef.current;
+    const content = this.contentRef.current;
+    this.popperInstance = createPopper(trigger, content, {
+      modifiers: [
+        {
+          name: 'offset',
+          options: {
+            offset: [0, 8],
+          },
+        }
+      ],
+    });
 
-		trigger.addEventListener('click', (evt) => {
-			this.state.popoverVisible ? this.hidePopover() : this.showPopover();
+    trigger.addEventListener('click', (evt) => {
+      this.state.popoverVisible ? this.hidePopover() : this.showPopover();
 
-			// Prevent event from bubbling to document body, thus preventing the popover from closing immediately
-			// after it opens, which to the user looks like the popover never opened
-			evt.stopPropagation();
-		});
+      // Prevent event from bubbling to document body, thus preventing the popover from closing immediately
+      // after it opens, which to the user looks like the popover never opened
+      evt.stopPropagation();
+    });
 
-		// Prevent click events from bubbling up to the document body. This prevents the popover from closing if
-		// the user clicks within its boundaries
-		content.addEventListener('click', (evt) => evt.stopPropagation());
-	}
+    // Prevent click events from bubbling up to the document body. This prevents the popover from closing if
+    // the user clicks within its boundaries
+    content.addEventListener('click', (evt) => evt.stopPropagation());
+  }
 
-	// Show popover, update position
-	showPopover = () => {
-		this.setState({ popoverVisible: true }, () => {
-			window.addEventListener('click', this.hidePopover);
-			this.contentRef.current.setAttribute('data-show', '');
-			this.popperInstance.update();
-		});
-	};
+  // Show popover, update position
+  showPopover = () => {
+    this.setState({ popoverVisible: true }, () => {
+      window.addEventListener('click', this.hidePopover);
+      this.contentRef.current.setAttribute('data-show', '');
+      this.popperInstance.update();
+    });
+  };
 
-	// Hide popover
-	hidePopover = () => {
-		this.setState({ popoverVisible: false }, () => {
-			window.removeEventListener('click', this.hidePopover);
-			this.contentRef.current.removeAttribute('data-show');
-		});
-	}
+  // Hide popover
+  hidePopover = () => {
+    this.setState({ popoverVisible: false }, () => {
+      window.removeEventListener('click', this.hidePopover);
+      this.contentRef.current.removeAttribute('data-show');
+    });
+  }
 
-	render() {
-		return (
-			<>
-				<div ref={this.triggerRef} className="popover-trigger">{this.props.trigger}</div>
-				<div ref={this.contentRef} className="popover-content" data-popper-placement={this.props.position}>
-					{this.props.content}
-					<div className="popover-arrow" data-popper-arrow />
-				</div>
-			</>
-		);
-	}
+  render() {
+    return (
+      <>
+        <div ref={this.triggerRef} className="popover-trigger">{this.props.trigger}</div>
+        <div ref={this.contentRef} className="popover-content" data-popper-placement={this.props.position}>
+          {this.props.content}
+          <div className="popover-arrow" data-popper-arrow />
+        </div>
+      </>
+    );
+  }
 }
 
 Popover.propTypes = {
-	trigger: PropTypes.object.isRequired,
-	content: PropTypes.object.isRequired,
-	position: PropTypes.string,
+  trigger: PropTypes.object.isRequired,
+  content: PropTypes.object.isRequired,
+  position: PropTypes.string,
 };
 
 Popover.defaultProps = {
-	position: 'left',
+  position: 'left',
 };
 
 export default Popover;
--- Popover/src/js/index.js
+++ Popover/src/js/index.js
@@ -3,32 +3,32 @@ import ReactDOM from 'react-dom';
 import Popover from './Popover';
 
 const itemStyle = {
-	width: 'calc(100% - 1em)',
-	padding: '0.5em',
+  width: 'calc(100% - 1em)',
+  padding: '0.5em',
 };
 
 const wrapperStyle = {
-	display: 'flex',
-	flexDirection: 'row',
-	justifyContent: 'space-evenly',
+  display: 'flex',
+  flexDirection: 'row',
+  justifyContent: 'space-evenly',
 };
 const triggerStyle = { padding: '8px', backgroundColor: 'grey' };
 
 const Example = () => (
-	<div style={wrapperStyle}>
-		<Popover
-			content={(
-				<div style={{ width: '10em', border: '1px solid black' }}>
-					<div style={itemStyle}>Item 1</div>
-					<div style={itemStyle}>Item 2</div>
-					<div style={itemStyle}>Item 3</div>
-				</div>
-			)}
-			trigger={<div style={triggerStyle}>Click me!</div>}
-		/>
-	</div>
+  <div style={wrapperStyle}>
+    <Popover
+      content={(
+        <div style={{ width: '10em', border: '1px solid black' }}>
+          <div style={itemStyle}>Item 1</div>
+          <div style={itemStyle}>Item 2</div>
+          <div style={itemStyle}>Item 3</div>
+        </div>
+      )}
+      trigger={<div style={triggerStyle}>Click me!</div>}
+    />
+  </div>
 );
 
 window.onload = function(){
-	ReactDOM.render(<Example />, document.getElementById('app'));
+  ReactDOM.render(<Example />, document.getElementById('app'));
 };
