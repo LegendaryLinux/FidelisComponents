18e9e72b57c3810211d98a7ba211fc0787696bcd
chris <chris@legendserver.info>
2024-04-03 14:24:25 -0400

    Dependency updates to all modules, and added docs to Utils.js

--- Utils/src/js/Utils.js
+++ Utils/src/js/Utils.js
@@ -6,6 +6,10 @@ import 'moment-timezone';
 import BannerMessage from '@fidelisppm/banner-message';
 
 class Utils {
+	/**
+	 * An object representing the US states
+	 * @type {Object<string, string>}
+	 */
 	static states = {
 		AL: 'Alabama',
 		AK: 'Alaska',
@@ -68,14 +72,23 @@ class Utils {
 		WY: 'Wyoming',
 	};
 
+	/**
+	 * Returns an object containing US states
+	 * @returns {Object<string, string>} - An object with state codes as keys and state names as values.
+	 */
 	static getStates() {
 		return this.states;
 	}
 
+	/**
+	 * Generate an optgroup of options for the selected states
+	 * @param {Array} selected - An array containing the selected states.
+	 * @returns {JSX.Element} - The generated state options wrapped in an optgroup.
+	 */
 	static getStateOptions(selected = []) {
 		const options = _map(Object.keys(this.states),
 			(state, id) => (
-				<option value={state} key={id} selected={selected.indexOf(state) !== -1}>
+				<option value={state} key={id} selected={selected.includes(state)}>
 					{this.states[state]}
 				</option>
 			));
@@ -87,46 +100,83 @@ class Utils {
 		);
 	}
 
+	/**
+	 * Retrieves an array of mini state options.
+	 * @returns {Array} An array of abbreviated state options.
+	 */
 	static getMiniStateOptions() {
 		return _map(Object.keys(this.states), state => <option value={state} key={state}>{state}</option>);
 	}
 
+	/**
+	 * Verifies if the given email appears valid.
+	 * @param {string} email - The email to be verified.
+	 * @returns {boolean} - Returns true if the email is valid, otherwise returns false.
+	 */
 	static verifyEmail(email) {
 		if (typeof (email) === 'undefined' || email === null) { return false; }
 		const regex = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
 		return regex.test(email);
 	}
 
+	/**
+	 * Verifies if a phone number appears valid.
+	 * @param {string} phone - The phone number to verify.
+	 * @return {boolean} - True if the phone number is valid, false otherwise.
+	 */
 	static verifyPhone(phone) {
 		if (typeof (phone) === 'undefined' || phone === null) { return false; }
 		const regex = /^[0-9]{10}$/;
 		return regex.test(phone);
 	}
 
+	/**
+	 * Verifies if a US zip code appears valid
+	 * @param {string} zip - The zip code to be verified.
+	 * @returns {boolean} - Returns true if the zip code is valid, false otherwise.
+	 */
 	static verifyZip(zip) {
 		if (typeof (zip) === 'undefined' || zip === null) { return false; }
 		const regex = /^[0-9]{5}$/;
 		return regex.test(zip);
 	}
 
+	/**
+	 * Verifies whether a zip code with optional plus-four code appears valid.
+	 * @param {string} zip - The zip code to be verified.
+	 * @returns {boolean} - True if the zip code is valid, otherwise false.
+	 */
 	static verifyZipPlusFour(zip) {
 		if (typeof (zip) === 'undefined' || zip === null) { return false; }
 		const regex = /^[0-9]{5}(-?[0-9]{4})?$/;
 		return regex.test(zip);
 	}
 
+	/**
+	 * Verifies if a given string contains a valid number.
+	 * @param {string} string - The string to be verified.
+	 * @returns {boolean} - True if the string represents a valid number, false otherwise.
+	 */
 	static verifyNumber(string) {
 		if (typeof (string) === 'undefined' || string === null) { return false; }
-		const regex = /^\d*$/;
-		return regex.test(string);
+		return /^\d*$/.test(string);
 	}
 
+	/**
+	 * Verifies if a string represents a valid decimal number.
+	 * @param {string} string - The string to be verified.
+	 * @returns {boolean} - Returns true if the string represents a valid decimal number, false otherwise.
+	 */
 	static verifyDecimal(string) {
 		if (typeof (string) === 'undefined' || string === null) { return false; }
 		const regex = /^\d(\d*|\.)+\d*$/;
 		return regex.test(string);
 	}
 
+	/**
+	 * Destroys any existing batter message
+	 * @return {void}
+	 */
 	static resetBannerMessage() {
 		// Delete any previous banner message's parent div
 		const oldBanner = document.getElementById('fidelis-banner-message-container');
@@ -138,6 +188,10 @@ class Utils {
 		document.body.insertBefore(bannerParent, document.body.firstChild);
 	}
 
+	/**
+	 * Displays a success banner with the given message.
+	 * @param {string} message - The message to display
+	 */
 	static showSuccessBanner(message) {
 		this.resetBannerMessage();
 		ReactDOM.render(
@@ -146,6 +200,10 @@ class Utils {
 		);
 	}
 
+	/**
+	 * Displays a warning banner with the given message.
+	 * @param {string} message - The message to display
+	 */
 	static showWarningBanner(message) {
 		this.resetBannerMessage();
 		ReactDOM.render(
@@ -154,6 +212,10 @@ class Utils {
 		);
 	}
 
+	/**
+	 * Displays an error banner with the given message.
+	 * @param {string} message - The message to display
+	 */
 	static showErrorBanner(message) {
 		this.resetBannerMessage();
 		ReactDOM.render(
@@ -162,12 +224,19 @@ class Utils {
 		);
 	}
 
+	/**
+	 * Checks if an array contains only unique elements.
+	 * Check is shallow, and based on equivalency rather than accuracy.
+	 * @param {array} arr - The input array to be checked.
+	 * @returns {boolean} - Returns true if the array contains only unique elements, otherwise returns false.
+	 * @deprecated
+	 */
 	static isArrayUnique(arr) {
 		const temp = [];
 		let unique = true;
 
 		_forEach(arr, (val) => {
-			if (temp.indexOf(val) > -1) {
+			if (temp.includes(val)) {
 				unique = false;
 			}
 
@@ -177,10 +246,15 @@ class Utils {
 		return unique;
 	}
 
+	/**
+	 *
+	 * @param param
+	 * @returns {*|null}
+	 */
 	static getParam(param) {
 		let value = null;
 		let thisParam = [];
-		location.search.substr(1).split('&').forEach((item) => {
+		location.search.substring(1).split('&').forEach((item) => {
 			thisParam = item.split('=');
 			if (thisParam[0] === param) {
 				value = decodeURIComponent(thisParam[1]);
@@ -190,6 +264,11 @@ class Utils {
 		return null;
 	}
 
+	/**
+	 * Generates a UUID (Universally Unique Identifier) based on the given parameter.
+	 * @param {number} a - The parameter used to generate the UUID.
+	 * @returns {string} - The generated UUID.
+	 */
 	static genUUID(a) {
 		return a ? (a ^ Math.random() * 16 >> a / 4).toString(16) : ([1e7] + -1e3 + -4e3 + -8e3 + -1e11)
 			.replace(/[018]/g, this.genUUID);
@@ -203,7 +282,7 @@ class Utils {
 	static makeReadableDate = dateString => <Moment date={dateString} interval={0} format="ddd MMM DD YYYY, h:mma" />;
 
 	/**
-	 * Transform a string into a readable phone number. If there are more then ten digits present in the
+	 * Transform a string into a readable phone number. If there are more than ten digits present in the
 	 * passed value, only the first ten will be returned
 	 * @param {string} phone
 	 * @returns {string}
